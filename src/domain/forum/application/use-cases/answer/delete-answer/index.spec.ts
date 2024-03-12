afaf68c77d3533c1cea5f5658e38189d7d992346
Felipe Freire <fellipefreiire3@gmail.com>
2024-03-12 14:06:57 -0300

    feat: events

--- src/domain/forum/application/use-cases/answer/delete-answer/index.spec.ts
+++ src/domain/forum/application/use-cases/answer/delete-answer/index.spec.ts
@@ -3,7 +3,7 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-r
 import { DeleteAnswerUseCase } from '.'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeAnswer } from 'test/factories/make-answer'
-import { NotAllowedError } from '../../errors/not-allowed-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { makeAnswerAttachment } from 'test/factories/make-answer-attachment'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
--- src/domain/forum/application/use-cases/question/fetch-question-comments/index.ts
+++ src/domain/forum/application/use-cases/question/fetch-question-comments/index.ts
@@ -1,4 +1,4 @@
-import { Either, right } from './../../../../../../core/either'
+import { Either, right } from '@/core/either'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 import { QuestionCommentsRepository } from '../../../repositories/question-comments-repository'
 
